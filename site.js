2eac1c7ce61f4bdda558d0b9b6555f7563133af5
forgetyan <yannick.forget+github@gmail.com>
2023-06-03 16:06:28 -0400

    Add ESLint + Contact form

--- site.js
+++ site.js
@@ -1,48 +1,48 @@
-(function() {
-    "use strict";
-  
-    /**
-     * Easy selector helper function
-     */
-    const select = (el, all = false) => {
-        el = el.trim()
-        if (all) {
-            return [...document.querySelectorAll(el)]
-        } else {
-            return document.querySelector(el)
-        }
-    }
+(function () {
+  'use strict'
 
-    /**
-     * Easy event listener function
-     */
-    const on = (type, el, listener, all = false) => {
-        let selectEl = select(el, all)
-        if (selectEl) {
-            if (all) {
-                selectEl.forEach(e => e.addEventListener(type, listener))
-            } else {
-                selectEl.addEventListener(type, listener)
-            }
-        }
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
     }
+  }
 
-    /**
-     * Easy on scroll event listener 
-     */
-    const onscroll = (el, listener) => {
-        el.addEventListener('scroll', listener)
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    const selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
     }
+  }
 
-    /**
+  /**
+   * Easy on scroll event listener
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
    * Navbar links active state on scroll
    */
-  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
-    let position = window.scrollY + 200
+    const position = window.scrollY + 200
     navbarlinks.forEach(navbarlink => {
       if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
+      const section = select(navbarlink.hash)
       if (!section) return
       if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
         navbarlink.classList.add('active')
@@ -54,97 +54,105 @@
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
 
-    /**
-     * Back to top button
-     */
-    let backtotop = select('.back-to-top')
-    if (backtotop) {
-        const toggleBacktotop = () => {
-            if (window.scrollY > 100) {
-                backtotop.classList.add('active')
-            } else {
-                backtotop.classList.remove('active')
-            }
-        }
-        window.addEventListener('load', toggleBacktotop)
-        onscroll(document, toggleBacktotop)
+  /**
+  * Back to top button
+  */
+  const backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
     }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
 
-    /**
+  on('submit', '.email-form', function (e) {
+    $('#submit-button').prop('disabled', true)
+  })
+
+  on('load', '#form-content', function (e) {
+    $('#email-step-1').hide()
+    $('#email-step-2').show()
+  })
+
+  /**
      * Mobile nav toggle
      */
-    on('click', '.mobile-nav-toggle', function(e) {
-        select('#navbar').classList.toggle('navbar-mobile')
-        this.classList.toggle('bi-list')
-        this.classList.toggle('bi-x')
-    })
+  on('click', '.mobile-nav-toggle', function (e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
 
-    /**
-     * Mobile nav dropdowns activate
-     */
-    on('click', '.navbar .dropdown > a', function(e) {
-        if (select('#navbar').classList.contains('navbar-mobile')) {
-        e.preventDefault()
-        this.nextElementSibling.classList.toggle('dropdown-active')
-        }
-    }, true)
-
-    /**
-     * Scrool with ofset on links with a class name .scrollto
-     */
-    on('click', '.scrollto', function(e) {
-        if (select(this.hash)) {
-        e.preventDefault()
-
-        let navbar = select('#navbar')
-        if (navbar.classList.contains('navbar-mobile')) {
-            navbar.classList.remove('navbar-mobile')
-            let navbarToggle = select('.mobile-nav-toggle')
-            navbarToggle.classList.toggle('bi-list')
-            navbarToggle.classList.toggle('bi-x')
-        }
-        scrollto(this.hash)
-        }
-    }, true)
-
-    /**
-     * Scrolls to an element with header offset
-     */
-    const scrollto = (el) => {
-        let header = select('#header')
-        let offset = header.offsetHeight
-
-        let elementPos = select(el).offsetTop
-        window.scrollTo({
-        top: elementPos - offset,
-        behavior: 'smooth'
-        })
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function (e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
     }
-    /**
-     * Toggle .header-scrolled class to #header when page is scrolled
-     */
-    let selectHeader = select('#header')
-    if (selectHeader) {
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function (e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+      const navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        const navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    const header = select('#header')
+    const offset = header.offsetHeight
+    const elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  const selectHeader = select('#header')
+  if (selectHeader) {
     const headerScrolled = () => {
-        if (window.scrollY > 100) {
-            selectHeader.classList.add('header-scrolled')
-        } else {
-            selectHeader.classList.remove('header-scrolled')
-        }
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
     }
     window.addEventListener('load', headerScrolled)
     onscroll(document, headerScrolled)
-    }
+  }
 
-    /**
-     * Animation on scroll
-     */
-    window.addEventListener('load', () => {
-        AOS.init({
-        duration: 500,
-        easing: "ease-in-out",
-        once: true,
-        mirror: false
-        });
-    });
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 500,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  })
 })()
